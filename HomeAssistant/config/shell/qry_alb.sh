f5327e8163f0d143c7fd5f61e24f5489c27a40f0
ynot123 <yland@personainternet.com>
2018-12-08 15:04:20 -0500

    Update qry_alb.sh

--- HomeAssistant/config/shell/qry_alb.sh
+++ HomeAssistant/config/shell/qry_alb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
-
-query=$(printf "artists 0 20 search:$1 tags:a \nexit\n" | nc localhost 9090 )
+HA_Token=Your_Long_Lived_Token_Here
+query=$(printf "artists 0 20 search:$1 tags:a \nexit\n" | nc lms_ipaddress 9090 )
 zero_chk=$query
 art_id=${query#artists*id%3A}
 art_id=${art_id%% artist*3A*}
@@ -18,10 +18,13 @@ then
     art_id="0"
     echo
     echo "Artist found so moving on to albums...."
-    query=$(printf "albums 0 20 search:$2 tags:al \nexit\n" | nc localhost 9090 )
+    query=$(printf "albums 0 20 search:$2 tags:al \nexit\n" | nc lms_ipaddress 9090 )
     zero_chk=$query
     alb_id=${query#album*tags*id%3A}
     alb_id=${alb_id%% album*3A*}
+    name=${query#album*album%3A}
+    name=${name%% artist*3A*}
+    name=$(echo -e "${name//%/\\x}")
     zero_chk=${zero_chk##album*count%3A}
     echo 
     echo "Album check zero is:" $zero_chk
@@ -39,10 +42,13 @@ then
 else
     echo
     echo "Artist found so moving on to albums...."
-    query=$(printf "albums 0 20 artist_id:$art_id search:$2 tags:al \nexit\n" | nc localhost 9090 )
+    query=$(printf "albums 0 20 artist_id:$art_id search:$2 tags:al \nexit\n" | nc lms_ipaddress 9090 )
     zero_chk=$query
     alb_id=${query#album*tags*id%3A}
     alb_id=${alb_id%% album*3A*}
+    name=${query#album*album%3A}
+    name=${name%% artist*3A*}
+    name=$(echo -e "${name//%/\\x}")
     zero_chk=${zero_chk##album*count%3A}
     echo 
     echo "Album check zero is:" $zero_chk
@@ -60,12 +66,15 @@ fi
 echo
 echo "Artist ID is:$art_id"
 echo "Album ID is: $alb_id"
+echo "Name is: $name"
 echo "now Posting results"
 echo
 
 
 
-curl -X POST -d '{"state":"'"${art_id}"'"}' https://HA_Link.duckdns.org/api/states/sensor.art_ID?api_password=HA_API_Password
+curl -X POST -d '{"state":"'"${art_id}"'"}' https://HA_Link.duckdns.org/api/states/sensor.art_ID? -H "Authorization Bearer $HA_Token"
 echo
-curl -X POST -d '{"state":"'"${alb_id}"'"}' https://HA_Link.duckdns.org/api/states/sensor.alb_ID?api_password=HA_API_Password
+curl -X POST -d '{"state":"'"${alb_id}"'"}' https://HA_Link.duckdns.org/api/states/sensor.alb_ID? -H "Authorization Bearer $HA_Token"
+echo
+curl -X POST -d '{"state":"'"${name}"'"}' https://HA_Link.duckdns.org/api/states/sensor.lms_name? -H "Authorization Bearer $HA_Token"
 echo
